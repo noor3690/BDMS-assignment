084b920e9dc5484e8a36e31c753ca789dfb2a82f
noor3690 <164336345+noor3690@users.noreply.github.com>
2024-03-21 04:33:44 -0600

    Add files via upload

---
+++ bdms.js
@@ -0,0 +1,8 @@
+var a = 8;
+var b = 4;
+console.log("a + b=", a + b);
+console.log("a - b=", a - b);
+console.log("a * b=", a * b);
+console.log("a / b=", a / b);
+console.log("a % b=", a % b);
+console.log("a ** b=", Math.pow(a, b));
---
+++ bdms.ts
@@ -0,0 +1,8 @@
+let a : number = 8;
+let b : number = 4;
+console.log("a + b=", a + b);
+console.log("a - b=", a - b);
+console.log("a * b=", a * b);
+console.log("a / b=", a / b);
+console.log("a % b=", a % b);
+console.log("a ** b=", a ** b);
